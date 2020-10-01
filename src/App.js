46a5ef235eb8a3aec16e6426ba7ee6a9c5524871
Ehsan Ali <71366898+mrehsan51@users.noreply.github.com>
2020-10-01 09:19:39 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+
+const App = () => {
+  return (
+    <main className="container">
+      <Navbar />
+    </main>
+  );
+};
+
+export default App;
